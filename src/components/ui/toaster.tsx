e27c81162cb33d93f7b5e5f5fbca9ded568e1982
RamssCR <ramsesgpm@gmail.com>
2025-06-10 21:08:38 -0500

    build: installed and created toaster

---
+++ src/components/ui/toaster.tsx
@@ -0,0 +1,29 @@
+import { 
+  Toaster as Sonner, 
+  type ToasterProps 
+} from "sonner"
+
+const Toaster = ({ ...props }: ToasterProps) => {
+
+  return (
+    <Sonner
+      className="toaster group"
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+        } as React.CSSProperties
+      }
+      position="bottom-right"
+      toastOptions={{
+        classNames: {
+          description: "text-popover-foreground",
+        }
+      }}
+      {...props}
+    />
+  )
+}
+
+export { Toaster }
