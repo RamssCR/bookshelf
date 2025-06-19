4218ac917c09b5b232a73123da097ac9fbe8f9c4
RamssCR <ramsesgpm@gmail.com>
2025-06-18 19:37:48 -0500

    fixed url redirection

--- src/components/book-details/RedirectTo.tsx
+++ src/components/book-details/RedirectTo.tsx
@@ -8,7 +8,7 @@ type RedirectToProps = {
 
 export const RedirectTo = ({ isYourBooks, slug }: RedirectToProps) => (
   <AppLink
-    to={`/${isYourBooks ? 'your-books' : 'discover'}/books/read/${slug}`}
+    to={`/${isYourBooks ? 'your-books' : 'discover'}/books/read/${slug}/1`}
     className="mt-3 w-fit group inline-flex items-center gap-0.5 text-base font-medium text-primary transition-all hover:underline"
   >
     {isYourBooks ? 'Continue Reading' : 'Read Book'}
