461f75290c9c83e84a4e40f5f027cf8de3654b5c
RamssCR <ramsesgpm@gmail.com>
2025-06-23 15:37:17 -0500

    feat: added aria attribute to user icon

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -17,7 +17,10 @@ export const Profile = () => {
         </section>
         <section className="w-full flex flex-col items-center gap-3 md:flex-row md:gap-5">
           <div className="p-2.5 size-36 flex items-center justify-center border-2 border-input rounded-full md:size-24">
-            <UserIcon className="fill-primary size-16 md:size-12" />
+            <UserIcon 
+              className="fill-primary size-16 md:size-12"
+              aria-hidden="true"
+            />
           </div>
           <article className="flex flex-col items-center gap-1 md:items-start">
             <Title as="h5" className="text-xl font-medium text-primary">{user?.username}</Title>
